31318e42ee6923bf8628f6ac256c8d4fde5bf710
Karanpreet Singh <114460519+karanpreetsingh462@users.noreply.github.com>
2025-08-15 12:04:34 +0530

    third

--- server/configs/db.js
+++ server/configs/db.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
 
-const connectDB=async()=>{
+const connectDB= async ()=>{
     try{
-        mongoose.connection.on('connected', ()=>console.log("Database connected"));
-        await mongoose.connect(`${process.env.MONGODB_URI}/hotel-booking`)
+        mongoose.connection.on('connected', ()=> console.log("Database connected"));
+        await mongoose.connect(`${process.env.MONGODB_URI}/Traveleur`)
     }
     catch(error){
         console.log(error.message);
--- server/server.js
+++ server/server.js
@@ -1,14 +1,14 @@
 import express from "express";
 import "dotenv/config";
 import cors from "cors";
-import connectDB from "./configs/db.js";
+import connectDB from "./configs/db.js"
 import { clerkMiddleware } from '@clerk/express'
 import clerkWebhooks from "./controllers/clerkWebhooks.js";
 
 connectDB()
 
     const app =express()
-    app.use(cors())
+    app.use(cors()) //enable cross-origin sharing of resource
 
     // middleware
     app.use(express.json())
